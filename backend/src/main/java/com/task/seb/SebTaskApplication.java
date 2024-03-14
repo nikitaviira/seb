ccf3b1321fe85177f65289891ed601db2c095602
Nikita Viira <aanikita62@gmail.com>
2024-03-14 15:22:10 +0200

    init project

---
+++ backend/src/main/java/com/task/seb/SebTaskApplication.java
@@ -0,0 +1,11 @@
+package com.task.seb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebTaskApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SebTaskApplication.class, args);
+	}
+}
