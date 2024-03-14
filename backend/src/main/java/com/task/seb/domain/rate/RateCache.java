145c7629720ef33e48df1e2dacf579dd7911501d
Nikita Viira <aanikita62@gmail.com>
2024-03-14 19:38:01 +0200

    add rate cache

---
+++ backend/src/main/java/com/task/seb/domain/rate/RateCache.java
@@ -0,0 +1,24 @@
+package com.task.seb.domain.rate;
+
+import com.task.seb.util.Currency;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class RateCache {
+    private static final String RATE_CACHE_NAME = "rate";
+    private final CurrencyRateRepository currencyRateRepository;
+
+    @Cacheable(value = RATE_CACHE_NAME, key = "#quote", unless = "#result.empty()")
+    public Optional<CurrencyRate> getLatestRate(Currency quote) {
+        return currencyRateRepository.findFirstByQuoteOrderByDateDesc(quote);
+    }
+
+    @CacheEvict(value = RATE_CACHE_NAME, key = "#quote")
+    public void evictRate(Currency quote) {}
+}
