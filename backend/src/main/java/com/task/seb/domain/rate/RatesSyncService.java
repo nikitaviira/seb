8d98672a4dde61509c21167b2954cc57090101d9
Nikita Viira <aanikita62@gmail.com>
2024-03-15 16:17:35 +0200

    filter out unknown currencies

--- backend/src/main/java/com/task/seb/domain/rate/RatesSyncService.java
+++ backend/src/main/java/com/task/seb/domain/rate/RatesSyncService.java
@@ -2,6 +2,7 @@ package com.task.seb.domain.rate;
 
 import com.task.seb.client.ExchangeRateType;
 import com.task.seb.client.FxRates;
+import com.task.seb.client.FxRates.FxRate;
 import com.task.seb.client.RatesApiClient;
 import com.task.seb.util.Currency;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +11,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static com.task.seb.client.ExchangeRateType.LT;
+import static com.task.seb.util.Currency.UNKNOWN;
 import static com.task.seb.util.DateUtil.today;
 
 @Service
@@ -39,7 +42,7 @@ public class RatesSyncService {
   @Transactional
   public void loadAndSaveLatestRates() {
     List<CurrencyRate> rates = fetchRates(() -> ratesApiClient.getCurrentFxRates(DEFAULT_EXCHANGE_RATE_TYPE))
-        .filter(this::isNewRate)
+        .filter(ignoreUnknownCurrencies().and(isNewRate()))
         .peek(rate -> rateService.evictRateCache(rate.getQuote()))
         .toList();
     currencyRateRepository.saveAll(rates);
@@ -49,13 +52,17 @@ public class RatesSyncService {
     return fetcher.get().getRates().stream().map(this::toCurrencyPairRate);
   }
 
-  private boolean isNewRate(CurrencyRate rate) {
-    return rateService.getLatestRate(rate.getQuote())
+  private Predicate<CurrencyRate> ignoreUnknownCurrencies() {
+    return rate -> rate.getQuote() != UNKNOWN;
+  }
+
+  private Predicate<CurrencyRate> isNewRate() {
+    return rate -> rateService.getLatestRate(rate.getQuote())
         .map(latestRate -> rate.getDate().isAfter(latestRate.getDate()))
         .orElse(false);
   }
 
-  private CurrencyRate toCurrencyPairRate(FxRates.FxRate fxRate) {
+  private CurrencyRate toCurrencyPairRate(FxRate fxRate) {
     var rate = new CurrencyRate();
     FxRates.CurrencyAmount quote = fxRate.getCurrencyAmounts().getLast();
     rate.setDate(fxRate.getDate());
