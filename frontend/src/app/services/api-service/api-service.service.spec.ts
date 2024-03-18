0a54d2513e3ba52e851b78ede0a5a94cda521ee1
Nikita Viira <aanikita62@gmail.com>
2024-03-18 22:52:05 +0200

    add tests for CurrencyApiService

--- frontend/src/app/services/api-service/api-service.service.spec.ts
+++ frontend/src/app/services/api-service/api-service.service.spec.ts
@@ -3,7 +3,7 @@ import {TestBed} from '@angular/core/testing';
 
 import {ApiService} from './api-service.service';
 
-fdescribe('ApiService', () => {
+describe('ApiService', () => {
   let service: ApiService;
   let httpMock: HttpTestingController;
 
--- frontend/src/app/services/currency-api/currency-api.service.spec.ts
+++ frontend/src/app/services/currency-api/currency-api.service.spec.ts
@@ -1,16 +1,52 @@
-import {TestBed} from '@angular/core/testing';
+import { HttpErrorResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 
-import {CurrencyApiService} from './currency-api.service';
+import { CurrencyApiService } from './currency-api.service';
 
-describe('CurrencyApiService', () => {
+fdescribe('CurrencyApiService', () => {
   let service: CurrencyApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CurrencyApiService]
+    });
     service = TestBed.inject(CurrencyApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('currencies should only be queried once when subscribed multiple times', () => {
+    service.currencies.subscribe();
+    service.currencies.subscribe();
+    service.currencies.subscribe();
+
+    // @ts-expect-error: protected access
+    const request = httpMock.expectOne(`${service.baseApiUrl}/api/currency-list`);
+    expect(request.request.method).toBe('GET');
+  });
+
+  it('on error should fetch again', () => {
+    service.currencies.subscribe({
+      error: (err) => expect(err).toBeInstanceOf(HttpErrorResponse)
+    });
+
+    // @ts-expect-error: protected access
+    httpMock.expectOne(`${service.baseApiUrl}/api/currency-list`).error();
+
+    service.currencies.subscribe();
+
+    // @ts-expect-error: protected access
+    const request = httpMock.expectOne(`${service.baseApiUrl}/api/currency-list`);
+    expect(request.request.method).toBe('GET');
   });
 });
