ff56199488ce73d176f0eed9d3c9b1a68ca7fe1a
Nikita Viira <aanikita62@gmail.com>
2024-03-19 00:41:14 +0200

    remove useless bean

--- backend/src/main/java/com/task/seb/client/FeignClientConfiguration.java
+++ backend/src/main/java/com/task/seb/client/FeignClientConfiguration.java
@@ -5,29 +5,20 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import feign.Feign;
-import feign.Logger.Level;
 import feign.Util;
 import feign.codec.Decoder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 import static com.fasterxml.jackson.databind.DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE;
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
-import static feign.Logger.Level.BASIC;
 import static feign.Util.UTF_8;
 
 @Configuration
 public class FeignClientConfiguration {
-    @Bean
-    @Profile("dev")
-    Level feignLoggerLevel() {
-        return BASIC;
-    }
-
     @Bean
     public Decoder feignDecoder() {
         return (response, type) -> {
